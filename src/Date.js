ed399a7df5864c8049caa3932826e6f7225a9133
olena-starova <iverra@ukr.net>
2022-10-02 00:30:38 +0300

    Delete old data

--- src/Date.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./Date.css";
-
-export default class Date extends React.Component {
-  render = () => {
-    return <p id="date">Sunday, October 2, 10:30</p>;
-  };
-}
