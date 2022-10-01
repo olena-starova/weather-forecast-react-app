dca8b0b55d3082a36f4d33678e48025155762994
olena-starova <iverra@ukr.net>
2022-10-01 20:09:15 +0300

    Update Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -4,6 +4,20 @@ import "./Weather.css";
 export default function Weather() {
   return (
     <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city"
+              className="form-control"
+            />
+            <div className="col-3">
+              <input type="submit" value="Search" className="btn btn-primary" />
+            </div>
+          </div>
+        </div>
+      </form>
       <h1>Kyiv</h1>
       <ul>
         <li>Sunday, 8 p.m.</li>
