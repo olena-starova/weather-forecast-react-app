6835062377350a07b3617922f9bdb5d80cfd28c2
olena-starova <iverra@ukr.net>
2022-10-01 23:15:09 +0300

    Update Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -30,7 +30,7 @@ export default function Weather(props) {
   }
 
   function search() {
-    const apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+    const apiKey = "d5c0155cd147c2d7c821980db5dc591e";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
   }
