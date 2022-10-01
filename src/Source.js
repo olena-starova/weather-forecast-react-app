ebaecd3608062367d060d3be76980e905c7b9fac
olena-starova <iverra@ukr.net>
2022-10-01 18:35:55 +0300

    Update Source.js

--- src/Source.js
+++ src/Source.js
@@ -6,19 +6,11 @@ export default class Source extends React.Component {
     return (
       <footer>
         This project was coded by Olena Starova and is{" "}
-        <a
-          href="https://github.com/olena-starova/weather-forecast-react-app"
-          target="_blank"
-        >
+        <a href="https://github.com/olena-starova/weather-forecast-react-app">
           open-sourced on GitHub
         </a>{" "}
         and hosted on{" "}
-        <a
-          href="https://transcendent-churros-77c234.netlify.app"
-          target="_blank"
-        >
-          Netlify
-        </a>
+        <a href="https://transcendent-churros-77c234.netlify.app">Netlify</a>
       </footer>
     );
   };
