49bd71f9fdc456dca06e86dc8b8833a6419fddcb
olena-starova <iverra@ukr.net>
2022-10-02 19:45:13 +0300

    New Icon

--- src/Weather.js
+++ src/Weather.js
@@ -15,6 +15,7 @@ export default function Weather(props) {
       humidity: response.data.main.humidity,
       wind: response.data.wind.speed,
       city: response.data.name,
+      iconUrl: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
     });
   }
   function handleSubmit(event) {
